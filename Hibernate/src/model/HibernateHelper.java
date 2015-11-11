87371e93b2a8a72e119305cf03a3743a161a4ee1
21443251 <21443251@N204-17.alumnos.uem.es>
2015-11-11 13:12:15 +0100

    clase 11 de noviembre

--- Hibernate/src/model/HibernateHelper.java
+++ Hibernate/src/model/HibernateHelper.java
@@ -1,24 +1,157 @@
 package model;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 public class HibernateHelper {
 	private SessionFactory sesion;
-	   public HibernateHelper(){         
-	          sesion = SessionFactoryUtil.getSessionFactory();
-	   }
-	   public void addDepartment(int id, String name, String city){
-	         Session session = sesion.openSession();
-	         Transaction tx = session.beginTransaction();         
-	         Departamento departamento = new Departamento(id,name,city, new HashSet<Empleado>(0));       
-	         session.save(departamento);       
-	         tx.commit();
-	         session.close();
-	   }
+
+	public HibernateHelper() {
+		sesion = SessionFactoryUtil.getSessionFactory();
+	}
+
+	public void addDepartment(int id, String name, String city) {
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		Departamento departamento = new Departamento(id, name, city,
+				new HashSet<Empleado>(0));
+		session.save(departamento);
+		tx.commit();
+		session.close();
+	}
+
+	public Departamento getDepartamento(int id) {
+
+		Session session = sesion.openSession();
+		Departamento dep = null;
+		if (id != -1) {
+			dep = (Departamento) session.get(Departamento.class, id);
+		}
+		if (dep == null) {
+			dep = new Departamento();
+		}
+
+		session.close();
+
+		return dep;
+	}
+
+	public void showDepEmp(int id) {
+		Session session = sesion.openSession();
+		// Obtener empleado
+		// Departamento dep = (Departamento) session.get(Departamento.class,
+		// id);
+		Departamento dep = null;
+		dep = (Departamento) session.get(Departamento.class, id);
+		if (dep != null) {
+			System.out.println("Cod: " + dep.getNumDept() + ", nombre: "
+					+ dep.getDnombre() + ", ciudad: " + dep.getCiudad());
+			// Obtener una lista de empleados del departamento
+			Set<Empleado> listaemp = dep.getEmpleados();
+			// Copiamos la lista a un iterador
+			Iterator<Empleado> it = listaemp.iterator();
+			System.out.println("Lista de empleados");
+			// Iterador recorre set para mostrar empleados
+			while (it.hasNext()) {
+				Empleado emp = new Empleado();
+				emp = it.next();
+				System.out.println(emp.getNumEmp() + " " + emp.getNombre());
+			}
+		} else {
+			System.out.println("no existe ese departamento");
+		}
+
+		session.close();
+	}
+
+	public void deleteDepartment(int id) {
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		Departamento dep = (Departamento) session.get(Departamento.class, id);
+		if (dep != null) {
+			System.out.println("departamento borrado");
+			session.delete(dep);
+			System.out.println("Borrado departamento");
+			tx.commit();
+		} else {
+			System.out.println("No existe el departamento " + id);
+		}
+
+		session.close();
+	}
+
+	public void updateDepartment(int id, String nomCity, String nomDept) {
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		Departamento dep = (Departamento) session.get(Departamento.class, id);
+
+		if (dep == null) {
+			System.out.println("No existe tal departamento");
+		} else {
+			dep.setCiudad(nomCity);
+			session.update(dep);
+		}
+		System.out.println("Cambio de la ciudad " + dep.getCiudad());
+
+		tx.commit();
+		session.close();
+	}
+
+	public List<Departamento> getDepartment2() {
+		// abrimos sesion
+		Session session = sesion.openSession();
+		// Creamos lista
+		List<Departamento> deps;
+		// crea consulta
+		Query query = session.createQuery("from Departamento");
+		// almacena el resultado
+		deps = query.list();
+		// cierra sesion
+		session.close();
+
+		return deps;
+	}
+
+	public void showDeps() {
+		Session session = sesion.openSession();
+		Departamento dep = new Departamento();
+		Query q = session.createQuery("from Departamento");
+		q.setFetchSize(10);
+		Iterator iter = q.iterate();
+		while (iter.hasNext()) {
+			dep = (Departamento) iter.next();
+			System.out.println(dep.getNumDept() + " " + dep.getDnombre() + " "
+					+ dep.getCiudad());
+		}
+		session.close();
+	}
+	
+	public void showUniqueDep(String nomDept){
+		Session session = sesion.openSession();
+		Departamento dep =  (Departamento) session.createQuery("from Departamento where dnombre =?")
+				.setString(0, nomDept).uniqueResult();
+		
+		System.out.println(dep.getNumDept()+" "+dep.getDnombre()+" "+dep.getCiudad());
+		session.close();
+	}
+	//Dummy method
+	public void averageSalario(){
+		Session session = sesion.openSession();
+		Query cons = session.createQuery("select avg(salario) from Empleado");
+	   	Double media = (Double) cons.uniqueResult();
+	   	System.out.println("Media:"+media);
+	   	session.close();
+	}
+	
+	//proyecto con todos estos métodos y una interfaz gráfica.
+	
+	//Falta insert,update,delete!!!! lo ven viernes 13
 
 }
-
--- Hibernate/src/model/Main.java
+++ Hibernate/src/model/Main.java
@@ -1,16 +1,63 @@
 package model;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	      HibernateHelper hibernateHelper =new HibernateHelper();
-	      System.out.println("Inserto un departamento nuevo");       
-	      hibernateHelper.addDepartment(40, "Technology","Berlin");
-	      
-	      
+		HibernateHelper hibernateHelper = new HibernateHelper();
+		/*
+		 * System.out.println("Inserto un departamento nuevo");
+		 * hibernateHelper.addDepartment(60, "Operations","London");
+		 */
 
+		/*
+		 * Departamento dep = hibernateHelper.getDepartamento(50);
+		 * System.out.println("Departamento 50 en "+
+		 * dep.getCiudad()+"\t se llama " +dep.getDnombre()); Set<Empleado>
+		 * empleadosDep= dep.getEmpleados();
+		 * 
+		 * Iterator<Empleado> iterador = empleadosDep.iterator();
+		 * while(iterador.hasNext()) iterador.next().getClass();
+		 */
+
+		/*
+		 * System.out.println("Mostrar empleados departamento: \n");
+		 * hibernateHelper.showDepEmp(10);
+		 */
+
+		// hibernateHelper.deleteDepartment(60);
+
+		// hibernateHelper.updateDepartment(50, "LONDON");
+
+		
+		//Consulta de departamentos mediante list
+		/*List<Departamento> lista = hibernateHelper.getDepartment2();
+
+		Iterator<Departamento> iter = lista.iterator();
+
+		Departamento dep = null;
+		while (iter.hasNext()) {
+			dep = iter.next();
+			System.out.println(dep.getNumDept() + " " + dep.getDnombre() + " "
+					+ dep.getCiudad());
+		}
+
+		
+		*/
+		//Consulta de departamentos mediante fetch
+		
+		//hibernateHelper.showDeps();
+		
+		//hibernateHelper.showUniqueDep("IT");
+		
+		//hibernateHelper.averageSalario();
+		
+		System.out.println("Fin del programa");
+		System.exit(0);
 	}
 
 }
-
